149cd272429087b9c33edcc0873a7486d861c253
rajithkonara <rajith.konara@tokoin.io>
2020-02-22 18:58:40 +0530

    init commit

---
+++ src/main/java/com/rkdevblog/ses/SimpleEmailServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rkdevblog.ses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleEmailServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleEmailServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rkdevblog/ses/SesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rkdevblog.ses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
