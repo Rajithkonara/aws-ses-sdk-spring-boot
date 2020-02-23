ba9248c8aebc07c09931360dc85a682d2ac0a994
rajithkonara <rajith.konara@tokoin.io>
2020-02-23 09:42:08 +0530

    creating the bean

--- src/main/java/com/rkdevblog/ses/configuration/AwsSesConfiguration.java
+++ src/main/java/com/rkdevblog/ses/configuration/AwsSesConfiguration.java
@@ -1,4 +1,28 @@
 package com.rkdevblog.ses.configuration;
 
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class AwsSesConfiguration {
+
+    private final String region;
+
+    public AwsSesConfiguration(@Value("${email.region}") String region) {
+        this.region = region;
+    }
+
+    /**
+     * Build the AWS ses client
+     *
+     * @return AmazonSimpleEmailServiceClientBuilder
+     */
+    @Bean
+    public AmazonSimpleEmailService amazonSimpleEmailService() {
+        return AmazonSimpleEmailServiceClientBuilder.standard()
+                .withRegion(region).build();
+    }
 }
