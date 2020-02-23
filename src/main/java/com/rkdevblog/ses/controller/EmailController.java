997071d1cbc84c3dc8c76df3d142b96c7dadd9f1
rajithkonara <rajith.konara@tokoin.io>
2020-02-23 09:42:48 +0530

    implementing the controller

--- src/main/java/com/rkdevblog/ses/controller/EmailController.java
+++ src/main/java/com/rkdevblog/ses/controller/EmailController.java
@@ -1,4 +1,36 @@
 package com.rkdevblog.ses.controller;
 
+import com.rkdevblog.ses.dto.EmailRequestDto;
+import com.rkdevblog.ses.exception.AwsSesClientException;
+import com.rkdevblog.ses.service.AwsSesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("api/v1/email")
 public class EmailController {
+
+    private final AwsSesService awsSesService;
+
+    @Autowired
+    public EmailController(AwsSesService awsSesService) {
+        this.awsSesService = awsSesService;
+    }
+
+    @PostMapping(value = "", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> sendEmail(@Valid @RequestBody EmailRequestDto emailRequestDto) {
+        try {
+            awsSesService.sendEmail(emailRequestDto.getEmail(), emailRequestDto.getBody());
+            return ResponseEntity.ok("Successfully Sent Email");
+        } catch (AwsSesClientException e) {
+            return ResponseEntity.status(500).body("Error occurred while sending email " + e);
+        }
+    }
 }
--- src/main/java/com/rkdevblog/ses/dto/EmailRequestDto.java
+++ src/main/java/com/rkdevblog/ses/dto/EmailRequestDto.java
@@ -2,9 +2,14 @@ package com.rkdevblog.ses.dto;
 
 import lombok.Getter;
 
-@Getter
-public class Email {
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
 
+@Getter
+public class EmailRequestDto {
+
+    @Email(message = "Invalid Email address")
     private String email;
+    @NotEmpty(message = "Email body cannot be Null")
     private String body;
 }
